ba2126d4d218ce5c78fdbd492836af8165a0f621
rojiel <121927778+rojiel@users.noreply.github.com>
2023-01-04 16:43:51 +0700

    test

--- Scripts/Leads Page/Script1672815648984.groovy
+++ Scripts/Leads Page/Script1672815648984.groovy
@@ -24,9 +24,7 @@ WebUI.verifyElementClickable(findTestObject('Leads Page/Page_Shortlyst_Project/a
 WebUI.click(findTestObject('Leads Page/Page_Shortlyst_Project/a_Projects'))
 
 WebUI.click(findTestObject('Leads Page/Page_Shortlyst_Project/button_Create New Project'))
-
-WebUI.delay(1)
-
+//WebUI.delay(1)
 WebUI.click(findTestObject('Leads Page/Page_Shortlyst_Project/input__project_name'))
 
 WebUI.setText(findTestObject('Leads Page/Page_Shortlyst_Project/input__project_name'), 'Testing Project')
